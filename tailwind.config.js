8ae4cc1765efa017c49587e09581edff6d11ea5b
Saurav Jaiswal <saurav14th@gmail.com>
2023-05-18 17:25:06 +0545

    National Insurance

---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+/** @type {import('tailwindcss').Config} */
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    screens: { sm: "480px", md: "768px", lg: "976px", xl: "1440px" },
+    extend: {
+      colors: {
+        cyan: "hsl(180, 66%, 49%)",
+        cyanLight: "hsl(180, 66%, 69%)",
+        darkViolet: "hsl(257, 27%, 26%)",
+        red: "hsl(0, 87%, 67%)",
+        grayishViolet: "hsl(257, 7%, 63%)",
+        veryDarkBlue: "hsl(255, 11%, 22%)",
+        veryDarkViolet: "hsl(260, 8%, 14%)",
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+      spacing: {
+        180: "32rem",
+      },
+    },
+  },
+  plugins: [],
+};
+// module.exports = {
+//   content: ["./**/*.{html,js}"],
+//   theme: {
+//     screens: {
+//       sm: "480px",
+//       md: "768px",
+//       lg: "976px",
+//       xl: "1440px",
+//     },
+//     extend: {
+//       colors: {
+//         strongCyan: "hsl(171, 66%, 44%)",
+//         lightBlue: "hsl(233, 100%, 69%)",
+//         darkGrayishBlue: "hsl(210, 10%, 33%)",
+//         grayishBlue: "hsl(201, 11%, 66%)",
+//       },
+//       fontFamily: {
+//         sans: ["Bai Jamjuree", "sans-serif"],
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
