bc0463ff91803fd5729277fb7e91e76f8c4463cd
Saurav Jaiswal <saurav14th@gmail.com>
2023-06-18 22:02:49 +0545

    responsive header

--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
-// Initialization for ES Users
-import { Carousel, initTE } from 'tw-elements'
+// const btn = document.getElementById('menu-btn')
+// const menu = document.getElementById('menu')
 
-initTE({ Carousel })
+// btn.addEventListener('click', toggleButton)
+
+// function toggleButton() {
+//   try {
+//     btn.classList.toggle('open')
+//     menu.classList.toggle('flex')
+//     menu.classList.toggle('hidden')
+//   } catch (err) {
+//     console.log(err)
+//   }
+// }
+
+let defaultTransform = 0
+function goNext() {
+  defaultTransform = defaultTransform - 398
+  let slider = document.getElementById('slider')
+  if (Math.abs(defaultTransform) >= slider.scrollWidth / 1.7)
+    defaultTransform = 0
+  slider.style.transform = 'translateX(' + defaultTransform + 'px)'
+}
+next.addEventListener('click', goNext)
+
+function goPrev() {
+  let slider = document.getElementById('slider')
+  if (Math.abs(defaultTransform) === 0) defaultTransform = 0
+  else defaultTransform = defaultTransform + 398
+  slider.style.transform = 'translateX(' + defaultTransform + 'px)'
+}
+
+prev.addEventListener('click', goPrev)
